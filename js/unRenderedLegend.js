de1946b90f446e8877c5d75826c421fa0eb6596d
soheilsaif <soheil.saifipour@ndm.ox.ac.uk>
2016-09-05 15:08:39 +0100

    Add a new legend for unRendered nodes

---
+++ js/unRenderedLegend.js
@@ -0,0 +1,89 @@
+/**
+ * Class responsible for keeping track of HPO terms and their properties, and for
+ * caching disorders data as loaded from the OMIM database.
+ * This information is graphically displayed in a 'Legend' box
+ *
+ * @class HPOLegend
+ * @constructor
+ */
+var unRenderedLegend = Class.create(unRenderedLegendSuper, {
+
+	initialize: function ($super) {
+		$super("UnRendered Nodes", true);
+		this._termCache = {};
+	},
+
+	_getPrefix: function (id) {
+		return "UnRendered";
+	},
+
+	/**
+	 * Retrieve the color associated with the given object
+	 *
+	 * @method getObjectColor
+	 * @param {String|Number} id ID of the object
+	 * @return {String} CSS color value for that disorder
+	 */
+	getObjectColor: function (id) {
+		return "#CCCCCC";
+	},
+
+	/**
+	 * Registers an occurrence of a phenotype.
+	 *
+	 * @method addCase
+	 * @param {Number|String} id ID for this term taken from the HPO database
+	 * @param {String} name The description of the phenotype
+	 * @param {Number} nodeID ID of the Person who has this phenotype
+	 */
+	addCase: function ($super, id, name, nodeID) {
+		if (!this._termCache.hasOwnProperty(id))
+			this._termCache[id] = new HPOTerm(id, name);
+
+		$super(id, name, nodeID);
+	},
+
+	addNode: function ($super, id, name, nodeID, node) {
+		if (!this._termCache.hasOwnProperty(id))
+			this._termCache[id] = { id:id, name:name};//new HPOTerm(id, name);
+
+		$super(id, name, nodeID, node);
+	},
+
+	/**
+	 * Updates the displayed phenotype name for the given phenotype
+	 *
+	 * @method _updateTermName
+	 * @param {Number} id The identifier of the phenotype to update
+	 * @private
+	 */
+	_updateTermName: function (id) {
+		//console.log("updating phenotype display for " + id + ", name = " + this.getTerm(id).getName());
+		var _this = this;
+		var name = this._legendBox.down('li#' + this._getPrefix() + '-' + _this._hashID(id) + ' .disorder-name');
+		name.update(this.getTerm(id).getName());
+	},
+
+	/**
+	 * Callback for dragging an object from the legend onto nodes
+	 *
+	 * @method _onDropGeneric
+	 * @param {Person} Person node
+	 * @param {String|Number} id ID of the phenotype being dropped
+	 */
+	_onDropObject: function (node, hpoID) {
+		if (node.isPersonGroup()) {
+			return;
+		}
+		var currentHPO = node.getHPO().slice(0);
+		if (currentHPO.indexOf(hpoID) == -1) {
+			currentHPO.push(hpoID);
+			editor.getView().unmarkAll();
+			var properties = { "setHPO": currentHPO };
+			var event = { "nodeID": node.getID(), "properties": properties };
+			document.fire("pedigree:node:setproperty", event);
+		} else {
+			this._onFailedDrag(node, "This person already has the selected phenotype", "Can't drag this phenotype to this person");
+		}
+	}
+});
