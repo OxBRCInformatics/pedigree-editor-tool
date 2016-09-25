c66be85bba699e9a3e89c2e7f0ae9d41c338e20a
soheilsaif <soheil.saifipour@ndm.ox.ac.uk>
2016-09-25 19:33:36 +0100

    Remove unUsed codes

--- js/Settings.js
+++ js/Settings.js
@@ -5,17 +5,7 @@
 Settings = Class.create({
 
 	initialize: function (baseURL) {
-		this._baseURL = baseURL ? baseURL : "http://localhost:8080/pedigree_editor";
-		this._settings = config; //load it from config.js
-	},
-
-	getAbsoluteURL: function (url) {
-		var _this = this;
-		return _this._baseURL + url;
-	},
-
-	setLoaded: function () {
-		this._loaded = true;
+		this._settings = config; //'config' is a global variable load it from config.js
 	},
 
 	getSetting: function (key) {
