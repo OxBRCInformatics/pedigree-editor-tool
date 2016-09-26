7a45cf94df257b4b0543ec7b07b3ac4b9f546c3c
soheilsaif <soheil.saifipour@ndm.ox.ac.uk>
2016-09-26 15:28:38 +0100

    Refactor congig file structure

--- rest/config.js
+++ rest/config.js
@@ -1,15 +1,16 @@
 var config = {
 	"codingDefinitionServiceEndpoint": "/OCService",
 	"diagramEndpoint": {
-		"service": "openclinica",
+		//"service": "openclinica",
+		//"pathToEditorJSFiles":"/includes/pedigreeEditor/"
 		//get path to pedigree directory like :
 		//"http://localhost:8083/openclinica/includes/pedigreeEditor/" or
 		//"https://gmc.genomicsengland.nhs.uk/rarediseases/demo/includes/pedigreeEditor/"
-		"pathToEditorJSFiles":"/includes/pedigreeEditor/"
 
 
-		//"service": "local"
 
+		"service": "local",
+		"pathToEditorJSFiles":"/"
 
 		//"service": "mercury",
 		//"mercuryHost": "http://MERCURY_ADDRESS.COM/gel/rare_diseases/1.3/pedigrees"
