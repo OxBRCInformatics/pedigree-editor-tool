c045cccaa6302756d95c6958251280894868bd79
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2016-10-11 16:26:28 +0100

    Update to version 1.0.4

--- rest/config.js
+++ rest/config.js
@@ -1,5 +1,5 @@
 var config = {
-	"version": "v1.0.3",
+	"version": "v1.0.4",
 	"codingDefinitionServiceEndpoint": "/codingDefinitionServiceEndpoint",
 	"diagramEndpoint": {
 		//"service": "openclinica",
