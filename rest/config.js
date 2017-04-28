482b31e8918a3bd2b051131726cb4b2647010ed3
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2017-04-28 14:02:52 +0100

    Improvements to the error handling messages
    Add a help desk contact block to error message
    Get the details from config

--- rest/config.js
+++ rest/config.js
@@ -1,21 +1,27 @@
 var config = {
-	"version": "v1.2.1-SNAPSHOT",
-	"codingDefinitionServiceEndpoint": "/codingDefinitionService",
-	"diagramEndpoint": {
-		//"service": "openclinica",
-		//"pathToEditorJSFiles":"/includes/pedigreeEditor/"
-		//get path to pedigree directory like :
-		//"http://localhost:8083/openclinica/includes/pedigreeEditor/" or
-		//"https://gmc.genomicsengland.nhs.uk/rarediseases/demo/includes/pedigreeEditor/"
+    "version": "v1.2.1-SNAPSHOT",
+    "codingDefinitionServiceEndpoint": "/codingDefinitionService",
+    // "helpdesk": {
+    //     "name": "Help Desk",
+    //     "contact": {
+    //         "phone": "0000000000",
+    //         "email": "help.desk@help.com"
+    //     }
+    // },
+    "diagramEndpoint": {
+        //"service": "openclinica",
+        //"pathToEditorJSFiles":"/includes/pedigreeEditor/"
+        //get path to pedigree directory like :
+        //"http://localhost:8083/openclinica/includes/pedigreeEditor/" or
+        //"https://gmc.genomicsengland.nhs.uk/rarediseases/demo/includes/pedigreeEditor/"
 
 
+        "pathToEditorJSFiles": "/",
+        "service": "local"
 
-		//"service": "local",
-		"pathToEditorJSFiles":"/",
-
-		"service": "mercury",
-		"mercuryHost": "http://localhost:8080/gel/rare_diseases/pedigrees"
-		//"pathToEditorJSFiles":"/includes/pedigreeEditor/"
-	},
-	"saveAndExit": true
+        //"service": "mercury",
+        //"mercuryHost": "http://localhost:8080/gel/rare_diseases/pedigrees"
+        //"pathToEditorJSFiles":"/includes/pedigreeEditor/"
+    },
+    "saveAndExit": true
 };
